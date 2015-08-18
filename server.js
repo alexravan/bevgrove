dc32df27c942100c9750c5f1af214367fb45ddcb
alexravan <alexravan@gmail.com>
2015-08-18 16:13:44 -0700

    front end

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://bevgrove:dv1000@ds035623.mongolab.com:35623/heroku_vphjj155';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/test';
+
+// var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://bevgrove:dv1000@ds035623.mongolab.com:35623/heroku_vphjj155';
+
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
