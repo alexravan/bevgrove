f6ab7c471bb56dfdf1d36c2c5d822a7ad051d274
Alex Ravan <alexravan@gmail.com>
2016-09-30 20:56:09 -0400

    python issue?

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var validator = require('validator');
 var app = express();
+var fs = require("fs");
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://127.0.0.1/test';
-
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://bg_heroku:dv1000@ds035623.mongolab.com:35623/heroku_vphjj155';
-
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/test';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
+
 
 
 
@@ -23,12 +23,13 @@ app.get('/', function(request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
 	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-	var indexPage = '';
-	indexPage += "<!DOCTYPE HTML><html><head><title>TBddD</title></head><body><h1>Welcome to TBD</h1>";				
-	indexPage += "</body></html>"
-					response.send(indexPage);
-	
+	// var indexPage = '';
+	// indexPage += "<!DOCTYPE HTML><html><head><title>f</title></head><body><h1>Welcome to TBD</h1>";				
+	// indexPage += "</body></html>"
+					// response.send(indexPage);
+	response.sendfile('public/index2.html');
 });
+
 
 app.get('/locations', function(request, response) {
 
@@ -53,6 +54,64 @@ app.get('/locations', function(request, response) {
 		});
 	});
 });
+
+
+ app.post('/addBusiness', function(request, response) {
+
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	
+	var business = String(request.body.business);
+	var cat = String(request.body.cat);
+	var img1 = String(request.body.img1);
+	var img2 = String(request.body.img2);
+	var img3 = String(request.body.img3);
+	var placeid = String(request.body.PlaceID);
+	var toInsert = 
+			{  "business" 	: business,
+			   "cat"		: cat,
+			   "img1" 		: img1,
+			   "img2" 		: img2,
+			   "img3" 		: img3,
+			   "PlaceID"	: placeid    
+			};
+		// {"error" : "Whoops, something is wrong with your data!"}
+	if (business && cat && placeid) {
+
+		db.collection('locations', function(error1, coll) {
+
+			coll.upsert( { business : business } , toInsert,  { upsert: true }, function(updateErr, updated) {
+
+				if (updateErr) {
+					response.send(500);
+				}
+				else {
+					
+						coll.find().toArray(function(err, info) {
+				 		if(!err)
+							response.send(info);
+				}); }
+		    });
+		});
+
+	}
+	else {
+		response.status(500);
+	}
+});
+
+
+app.get('/addBusiness', function(request, res) {
+    fs.readFile('form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+                // 'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+});
+
 
 app.use(express.static('public'));
 
